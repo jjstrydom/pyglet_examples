15ab0eca74f9dea564b95675d63dff9c31b185db
Jurgen Strydom <jurgen.strydom@gmail.com>
2017-04-30 15:55:29 +0200

    Commit of Pyglet OpenGL tutorial by DLC ENERGY

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ A repository with pyglet examples that I have come across and tried implementing
 
 ## Youtube [Pyglet OpenGL tutorial by DLC ENERGY](https://www.youtube.com/watch?v=Hqg4qePJV2U)
 
-Code as taught by Aiden Ward on his youtube channel.
-
+Code as taught by Aiden Ward on his youtube channel. Code in minecraft_block.py
---
+++ minecraft_block.py
@@ -0,0 +1,148 @@
+# code from https://www.youtube.com/watch?v=Hqg4qePJV2U
+# Pyglet OpenGL tutorial by DLC ENERGY
+# As taught by Aiden Ward
+
+import pyglet
+from pyglet.gl import *
+from pyglet.window import key
+import math
+
+class Model:
+
+    def get_tex(self,file):
+        tex = pyglet.image.load(file).texture
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        return pyglet.graphics.TextureGroup(tex)
+
+    def __init__(self):
+
+        self.top = self.get_tex('grass_top.png')
+        self.side = self.get_tex('grass_side.png')
+        self.bottom = self.get_tex('dirt.png')
+
+        self.batch = pyglet.graphics.Batch()
+
+        tex_coords = ('t2f', (0,0, 1,0, 1,1, 0,1))
+
+        x, y, z = 0, 0 ,-1
+        X, Y, Z = x+1, y+1, z+1
+
+        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (X, y, z,  x, y, z,  x, Y, z,  X, Y, z)), tex_coords) # back
+        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (x, y, Z,  X, y, Z,  X, Y, Z,  x, Y, Z)), tex_coords) # front
+
+        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (x, y, z,  x, y, Z,  x, Y, Z,  x, Y, z)), tex_coords)  # left
+        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (X, y, Z,  X, y, z,  X, Y, z,  X, Y, Z)), tex_coords)  # right
+
+        self.batch.add(4, GL_QUADS, self.bottom, ('v3f', (x, y, z,  X, y, z,  X, y, Z,  x, y, Z)), tex_coords)  # bottom
+        self.batch.add(4, GL_QUADS, self.top,    ('v3f', (x, Y, Z,  X, Y, Z,  X, Y, z,  x, Y, z)), tex_coords)  # top
+
+    def draw(self):
+        self.batch.draw()
+
+class Player:
+    def __init__(self, pos=(0, 0, 0), rot=(0, 0)):
+        self.pos = list(pos)
+        self.rot = list(rot)
+
+    def mouse_motion(self, dx, dy):
+        dx/= 8
+        dy/= 8
+        self.rot[0] += dy
+        self.rot[1] -= dx
+        if self.rot[0]>90:
+            self.rot[0] = 90
+        elif self.rot[0] < -90:
+            self.rot[0] = -90
+
+    def update(self,dt,keys):
+        s = dt*10
+        rotY = -self.rot[1]/180*math.pi
+        dx, dz = s*math.sin(rotY), math.cos(rotY)
+        if keys[key.W]:
+            self.pos[0] += dx
+            self.pos[2] -= dz
+        if keys[key.S]:
+            self.pos[0] -= dx
+            self.pos[2] += dz
+        if keys[key.A]:
+            self.pos[0] -= dz
+            self.pos[2] -= dx
+        if keys[key.D]:
+            self.pos[0] += dz
+            self.pos[2] += dx
+        if keys[key.SPACE]:
+            self.pos[1] += s
+        if keys[key.LSHIFT]:
+            self.pos[1] -= s
+
+class Window(pyglet.window.Window):
+
+    def push(self,pos,rot):
+        glPushMatrix()
+        rot = self.player.rot
+        pos = self.player.pos
+        glRotatef(-rot[0],1,0,0)
+        glRotatef(-rot[1],0,1,0)
+        glTranslatef(-pos[0], -pos[1], -pos[2])
+
+    def Projection(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+    def Model(self):
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+    def set2d(self):
+        self.Projection()
+        gluPerspective(0, self.width, 0, self.height)
+        self.Model()
+
+    def set3d(self):
+        self.Projection()
+        gluPerspective(70, self.width/self.height, 0.05, 1000)
+        self.Model()
+
+    def setLock(self, state):
+        self.lock = state
+        self.set_exclusive_mouse(state)
+
+    lock = False
+    mouse_lock = property(lambda self:self.lock, setLock)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_minimum_size(300,200)
+        self.keys = key.KeyStateHandler()
+        self.push_handlers(self.keys)
+        pyglet.clock.schedule(self.update)
+
+        self.model = Model()
+        self.player = Player((0.5,1.5,1.5),(-30,0))
+
+    def on_mouse_motion(self,x,y,dx,dy):
+        if self.mouse_lock: self.player.mouse_motion(dx,dy)
+
+    def on_key_press(self, KEY, _MOD):
+        if KEY == key.ESCAPE:
+            self.close()
+        elif KEY == key.E:
+            self.mouse_lock = not self.mouse_lock
+
+    def update(self, dt):
+        self.player.update(dt, self.keys)
+
+    def on_draw(self):
+        self.clear()
+        self.set3d()
+        self.push(self.player.pos,self.player.rot)
+        self.model.draw()
+        glPopMatrix()
+
+if __name__ == '__main__':
+    window = Window(width=400, height=300, caption='My caption',resizable=True)
+    glClearColor(0.5,0.7,1,1)
+    glEnable(GL_DEPTH_TEST)
+    #glEnable(GL_CULL_FACE)
+    pyglet.app.run()
